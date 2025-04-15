4c52a38ea609ce1a9c7b189f7a38319d28e581a2
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-04-15 12:58:00 -0500

    Removes accidental personal setting for printWidth

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,5 @@
 /** @type {import("prettier").Config} */
 export default {
-  printWidth: 100,
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   tailwindFunctions: ["tv"],
   overrides: [
