2357359682801adfaa74a2968cc8c38102c7c20d
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-10-16 18:24:43 -0500

    Fixes missing setting in .prettierrc.mjs for prettier.plugin.tailwindcss

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,7 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/styles/starwind.css",
   tailwindFunctions: ["tv"],
   overrides: [
     {
